f78ccbaa8345bab588371977f55b497fc9d0a627
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-20 21:48:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -10,8 +10,7 @@ end
 
 def display_card_total(card_total)
   # code #display_card_total here
-  puts "Your cards add up to #{card total}"
-  
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
