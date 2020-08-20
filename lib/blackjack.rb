b177b9f2f0a7dcb1f2b40b3531cbb4eb7e167b16
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-20 22:36:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -45,7 +45,8 @@ def hit?(current_card_total)
   if user_input ===  'h'
     total = deal_card 
     current_card_total += total
-  if
+  else if user_input === 's'
+  
   end
   current_card_total
 end
